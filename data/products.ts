fd9fb2d9d55698c53978fd563ad46fc82d62b3a0
DeboraQuinteros <debo-cab@hotmail.es>
2026-01-29 16:43:39 -0300

    categorias

--- data/products.ts
+++ data/products.ts
@@ -3,13 +3,15 @@ export const PRODUCTS = [
     id: 1,
     name: "Mate Imperial de Lujo",
     price: 45000,
+    category: "Mates", // <--- Agregamos esto
     description: "Cuero legítimo y virola de alpaca cincelada.",
     image: "/mate.jpg"
   },
   {
     id: 2,
-    name: "Termo Edición Black",
+    name: "Set Matero Edición Black",
     price: 95000,
+    category: "Set Materos", // <--- Agregamos esto
     description: "Acero inoxidable, mantiene 24hs el calor.",
     image: "/termo.jpg"
   },
@@ -17,6 +19,7 @@ export const PRODUCTS = [
     id: 3,
     name: "Bombilla de Plata",
     price: 12000,
+    category: "Bombillas", // <--- Agregamos esto
     description: "Filtro tipo resorte, máxima calidad.",
     image: "/bombilla.jpg"
   }
